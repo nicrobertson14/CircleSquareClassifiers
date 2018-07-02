4fc8da5398e5ed421cec4528f21c40b5a8a00483
Nicholas Robertson <nicrobertson14@hotmail.com>
2018-07-03 01:20:14 +0930

    subdirectories

---
+++ src/DT.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import numpy as np
+import scipy.io
+import matplotlib
+import matplotlib.pyplot as plt
+import math
+import copy
+import time
+
+tree = None
+
+class Node():
+    def __init__(self,att,split):
+        self.attribute = att
+        self.split = split
+        self.left = self.right = None
+
+def frange(lower,upper,step,div):
+    return [x / div for x in range(lower, upper, step)]
+
+def mode(image_tuples):
+    count = 0
+    for tuple in image_tuples:
+        count += tuple[1] 
+        if(count > len(image_tuples)/2):
+            return 1
+    return 0
+
+def Entropy(image_tuples):
+    c1 = 0
+    size = len(image_tuples)
+    for tuple in image_tuples:
+        c1 += tuple[1]
+    c2 = size - c1
+    
+    # check for perfect split
+    if(c1 * c2 == 0):
+        return 0
+    return -1 * (c1/size) * math.log2(c1/size) - (c2/size) * math.log2(c2/size)
+
+def InformationGain(before,left,right):
+    size = len(before)
+    ent_before = Entropy(before)
+    ent_left = Entropy(left)
+    ent_right=Entropy(right)
+    ent_after = (len(left)/size) * ent_left + (len(right)/size)*ent_right
+    
+    return ent_before - ent_after
+
+# Iterates through possible thresholds/attributes to find highest information gain
+def ChooseAttribute(attributes,image_tuples):
+    thresholds = frange(1,100,2,100.0)
+    best_ig = -1
+    less = []
+    greater = []
+    
+    for attribute in attributes:
+        for threshold in thresholds:
+            for tuple in image_tuples:
+                if(tuple[0][attribute] >= threshold):
+                    greater.append(tuple)
+                else:
+                    less.append(tuple)
+            inf_gain = InformationGain(image_tuples,less,greater)
+            if(inf_gain > best_ig):
+                best_ig = inf_gain
+                best_att = attributes.index(attribute)
+                best_left = copy.copy(less)
+                best_right = copy.copy(greater)
+                best_thresh = threshold
+            less.clear()
+            greater.clear()
+            
+    return [best_att,best_thresh,best_left,best_right]
+
+# checks if all labels in a list are the same
+def CheckLabels(image_tuples):
+    for i in range(len(image_tuples)-1):
+       if(image_tuples[i][1] != image_tuples[i+1][1]):
+           return False
+       
+    return True
+
+# Recursively builds a decision tree, split by attribute with greatest information gain
+def DTL(attributes,image_tuples,default,prune_factor):
+    if(len(image_tuples) == 0):
+        return default
+    elif(len(attributes) == 0):
+        return mode(image_tuples)
+    elif(Entropy(image_tuples) < prune_factor):
+        return mode(image_tuples)
+    elif(CheckLabels(image_tuples) == True):
+        return image_tuples[0][1]
+    else:
+        best_attr,split,left,right = ChooseAttribute(attributes,image_tuples)
+        tree = Node(best_attr,split)
+        
+        #remove best attribute
+        del attributes[best_attr]
+        tree.left = DTL(attributes,left,mode(image_tuples),prune_factor)
+        tree.right= DTL(attributes,right,mode(image_tuples),prune_factor)
+        
+        return tree
+
+# Recursively searches tree returning predicted label
+def SearchDT(tree,image_tuple):
+    if(tree == 0 or tree == 1):
+        return tree
+    elif(image_tuple[0][tree.attribute] < tree.split):
+        return SearchDT(tree.left,image_tuple)
+    else:
+        return SearchDT(tree.right,image_tuple)
+    
+def TestClassifier(image_tuples):
+    global tree
+    correct_count = 0
+    for tuple in image_tuples:
+        best = SearchDT(tree,tuple)
+        if(best == tuple[1]):
+            correct_count += 1
+
+    return correct_count/len(image_tuples)
+    
+
+
+dataset = scipy.io.loadmat('dataset.mat')
+trainImages = dataset['train_image'].reshape(200,576)
+trainLabels = dataset['train_label']
+testImages = dataset['test_image'].reshape(200,576)
+testLabels = dataset['test_label']
+train_tuples = []
+test_tuples = []
+for i in range(0,200,1):
+    train_tuples.append([trainImages[i,:],trainLabels[i][0]])
+    test_tuples.append([testImages[i,:],testLabels[i][0]])
+    
+tree = DTL(list(range(0,576)),train_tuples,None,0.0)
+times = []
+for i in range(10):
+    t0 = time.clock()
+    TestClassifier(test_tuples)
+    times.append(time.clock()-t0)
+
+print(np.median(times))
+
---
+++ src/knn.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import scipy.io
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from scipy import spatial
+import math
+from operator import itemgetter
+import time
+
+#global variables
+tree = None
+neighbours = ()
+
+class Node():
+    def __init__(self,value):
+        self.data = value
+        self.left = self.parent = self.right = None
+
+# maintains a sorted list of k nearest neighbours
+def addNeighbour(nodetuple):
+    global neighbours
+    if(nodetuple[0] >= neighbours[-1][0]):
+        return
+    for neighbour in neighbours:
+        if(nodetuple[0] == neighbour[0]):
+            return
+    neighbours[-1] = nodetuple
+    neighbours = sorted(neighbours, key=itemgetter(0))
+
+# returns the euclidean distance between two images
+def Euclidean(image1,image2):
+    sqDiff = np.square(image1-image2)
+    sum = np.sum(sqDiff)
+    return np.sqrt(sum)
+    
+# returns the lower bound distance between a node and query point
+def distanceLB(node,query):
+    return abs(node.image[node.d] - query[node.d])
+
+# returns the median value of a list
+def getMedian(list):
+    copy = list.copy()
+    copy.sort()
+    index = int(len(copy)/2)
+    return [copy[index]]
+
+# checks if input node is a leaf
+def isLeaf(node):
+    return node.left == None and node.right == None
+    
+# recursively builds k-d tree, sorting and splitting data at median value
+def makeKdTree(images,labels,feature):
+    size = len(images)
+    if(size == 0):
+        return
+
+    idx = np.argsort(images[:,feature])
+    sorted_images = images[idx,:]
+    sorted_labels = labels[idx,:]
+    median = getMedian(sorted_images[:,feature])
+    median_index = int(len(sorted_images[:,feature])/2) 
+
+    root = Node(None)
+    root.data = median
+    root.d = feature
+    root.image = sorted_images[median_index,:]
+    root.label = sorted_labels[median_index,:][0]
+
+
+    if(size == 1):
+        root.leaf = "LEAF"
+#        root.label = sorted_labels[median_index,:][0]
+        return root
+    
+    root.left = makeKdTree(sorted_images[0:median_index,:],sorted_labels[0:median_index,:],feature+1)
+    root.right = makeKdTree(sorted_images[median_index:size,:],sorted_labels[median_index:size,:],feature+1)
+
+    return root
+
+# recursively searches for closest image to query image
+def searchKdTree(root,input,best):
+    global neighbours
+    if(root == None):
+        return best
+    if(isLeaf(root)):
+        addNeighbour((Euclidean(root.image,input),root.label))
+        if(best == -1):
+            return root
+    
+    if(input[root.d] < root.data):
+        child_near = root.left
+        child_far = root.right
+    else:
+        child_near = root.right
+        child_far = root.left
+    
+    best = searchKdTree(child_near,input,best)
+    last = len(neighbours) - 1
+    if(distanceLB(root,input) < neighbours[last][0] or neighbours[last][0] == math.inf):
+        best = searchKdTree(child_far,input,best)
+
+    return best
+
+# predicts image label based on majority vote from k nearest neighbours
+def knnVote(neighbours):
+    size = len(neighbours)
+    count = 0
+#    print(neighbours)
+    for neighbour in neighbours:
+        if(neighbour[1] == 1):
+            count += 1
+
+    return count > int(size/2)
+
+# searches k-d tree for knn of each test image and prints prediction accuracy
+def testClassifier(images,labels,k):
+    global tree, neighbours
+    accuracy = []
+    for i in range(labels.size):
+        neighbours = [(math.inf,math.inf)] * k
+        searchKdTree(tree,images[i,:],-1)
+
+        if(knnVote(neighbours) == labels[i,:][0]):
+            accuracy.append(1)
+        else:
+            accuracy.append(0)
+        neighbours.clear()
+    
+    return np.sum(accuracy)/len(accuracy)
+
+dataset = scipy.io.loadmat('dataset.mat')
+trainImages = dataset['train_image'].reshape(200,576)
+trainLabels = dataset['train_label']
+testImages = dataset['test_image'].reshape(200,576)
+testLabels = dataset['test_label']
+
+
+tree = makeKdTree(trainImages,trainLabels,0)
+testClassifier(testImages,testLabels,2)
