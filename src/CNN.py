06b77fa123f0bfe0da246c165e2f2fc1bf4b6400
nicrobertson14 <40709715+nicrobertson14@users.noreply.github.com>
2018-07-03 15:50:12 +0930

    Update CNN.py

--- src/CNN.py
+++ src/CNN.py
@@ -15,21 +15,24 @@ from keras.models import Sequential
 from keras.layers import Dense, Flatten
 from keras import backend as K
 
+# the data, split between train and test sets
 dataset = scipy.io.loadmat('dataset.mat')
-trainImages = dataset['train_image'].reshape(200,576)
-trainLabels = dataset['train_label']
-testImages = dataset['test_image'].reshape(200,576)
-testLabels = dataset['test_label']
+x_train = dataset['train_image'].reshape(200,576)
+y_train = dataset['train_label']
+x_test = dataset['test_image'].reshape(200,576)
+y_test = dataset['test_label']
+
+# input image dimensions
+num_images = len(x_train)
+img_rows, img_cols = 24, 24
+num_pixels = img_rows * img_cols
 
 # batch size for gradient descent
-batch_size = 32
-# number of classes
+batch_size = 16
+# number of image labels (Squares & Circles)
 num_classes = 2
-# number of epochs (1 epoch = amount of iterations that covers the whole training set)
-epochs = 200 # try a larger number of epochs here (for example 10 or larger)
-# input image dimensions
-nmb_samples, img_rows, img_cols = x_train.shape[0], x_train.shape[1], x_train.shape[2]
-nmb_test_samples = x_test.shape[0]
+# number of epochs
+epochs = 200
 
 # adjust training image format
 if K.image_data_format() == 'channels_first':
@@ -44,35 +47,38 @@ else:
 # type casting and dimensionality transformations
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
-x_train = x_train.reshape(nmb_samples, img_rows*img_cols)
-x_test = x_test.reshape(nmb_test_samples, img_rows*img_cols)
 
-# convert class vectors to binary class matrices (one hot vectors)
-y_train = keras.utils.to_categorical(np.squeeze(y_train), num_classes)
-y_test = keras.utils.to_categorical(np.squeeze(y_test), num_classes)
 
-# define MLP model (2 hidden layers with 512 nodes, activated by ReLUs)
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
 model = Sequential()
-model.add(Dense(512, activation='relu', input_shape=(img_rows*img_cols,)))
-model.add(Dense(512, activation='relu'))
+model.add(Conv2D(6, kernel_size=(5, 5),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(16, (5, 5), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(120, activation='relu'))
+model.add(Dense(84, activation='relu'))
 model.add(Dense(num_classes, activation='softmax'))
 
-# compile model
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adadelta(),
               metrics=['accuracy'])
 
-# training
 history = model.fit(x_train, y_train,
               batch_size=batch_size,
               epochs=epochs,
-              verbose=1,
+              verbose=0,
               validation_data=(x_test, y_test))
 
-# print train and test losses and classification accuracies
 score = model.evaluate(x_train, y_train, verbose=0)
 print('Train loss:', score[0])
 print('Train accuracy:', score[1])
+
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
